a902e67ffc4f40556943a1e7b0060cba3b5faeac
Mike Ignaczak <mikeignaczak@gmail.com>
2018-02-27 00:17:58 -0600

    pushing

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+// Data
+// =============================================================================
+module.exports = [
+  {
+    name: 'Mike Ignaczak',
+    photo: 'https://media.giphy.com/media/sX6AUAj18Wztu/giphy.gif',
+    scores: ['2', '5', '5', '4', '5', '1', '1', '2', '2', '2'],
+  },
+  {
+    name: 'Arnold Schwarzenegger',
+    photo: 'https://media.giphy.com/media/7oMx9hEaUaMZa/giphy.gif',
+    scores: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+  },
+  {
+    name: 'Michael Jordan',
+    photo: 'https://media.giphy.com/media/60Ds0zREQt4MU/giphy.gif`',
+    scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5'],
+  },
+];
